a93ac9d0396cff741815ba8a98492726fb281e9e
Taylor Zanke <taylor.zanke@gmail.com>
2020-08-14 14:42:43 -0700

    image mobile

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid'
 import './Hero.scss'
+import ReactPlayer from 'react-player'
 
 import heroVideo from '../../assets/videos/hero-video.mp4'
+import heroImage from '../../assets/images/hero-image.png'
 
 const Hero = () => (
     <Grid container className="hero-container">
         <Grid item xs={12} className="video-wrapper">
-            <video className="hero-video" autoPlay loop muted src={heroVideo}/>
+            {/* <ReactPlayer 
+                url={heroVideo}
+                muted={true}
+                controls={false}
+                loop={true}
+                playing={true}
+                playsineline={true}
+                className="hero-video"
+            /> */}
+            <video className="hero-video" autoPlay muted loop src={heroVideo}/>
+            <img className="hero-image" src={heroImage}/>
         </Grid>
     </Grid>
 )
