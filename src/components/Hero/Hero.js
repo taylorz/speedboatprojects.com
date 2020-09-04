caa5a88cebd61c347d260b25894b01ec7c37b878
Taylor Zanke <taylor.zanke@gmail.com>
2020-09-04 15:26:17 -0700

    hero, desktop

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,6 +9,9 @@ const Hero = () => (
         <Grid item xs={12} className="video-wrapper">
             <video className="hero-video" autoPlay loop muted src={heroVideo}/>
         </Grid>
+        <Grid item xs={12} className="text-wrapper">
+            <div className="hero-text">SPEEDBOAT is a dynamic and multifaceted design & construction management studio. We create projects informed by timeless architectural concepts and supported by spatial clarity, detail, and craft. Our unique approach enables the imagination of exceptional clients and makes building projects more fun.</div>
+        </Grid>
     </Grid>
 )
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Link, NavLink} from 'react-router-dom'
 import './Nav.scss'
 import Grid from '@material-ui/core/Grid'
@@ -13,8 +13,6 @@ const Nav = ({ onClick }) => (
                 <Grid item className="nav-items">
                     <ul>
                         <li><NavLink exact onClick={onClick} to="/" className="page-link" activeClassName="page-link-active">Speedboat</NavLink></li>
-                        <li><NavLink exact onClick={onClick} to="/studio" className="page-link" activeClassName="page-link-active">Studio</NavLink></li>
-                        <li><NavLink exact onClick={onClick} to="/contact" className="page-link" activeClassName="page-link-active">Contact</NavLink></li>
                     </ul>
                 </Grid>
                 <Grid item className="nav-cta">
