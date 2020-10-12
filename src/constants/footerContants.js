d8c1bee0fc08b32adddce93e470f7c3f3cd27c46
Vincent Dandenault <vdandenault@gmail.com>
2020-10-12 11:30:43 -0400

    fix: :bug: Fix names in Partners

--- src/constants/footerContants.js
+++ src/constants/footerContants.js
@@ -34,29 +34,51 @@ const FOOTER_CONTENT = {
                 },
         ],
         People: {
-            title: 'PEOPLE', 
+            title: 'PARTNERS', 
             peoples: [{
-                text : 'Chad Karty (American; b. 1985, Detroit, MI; he/him/his) is an architectural professional delivering unique projects to clients in the arts, \
+                name: 'Chad Karty', 
+                text : ' is an architectural professional delivering unique projects to clients in the arts, \
                 entertainment, technology, & finance industries. In the past he worked as a carpenter, fabricating contemporary artworks and restoring Greek \
                 Revival townhomes. He earned his B.A. and M.Arch at Columbia University, where he received honors and taught design in the New York–Paris Studio. \
                 Before forming Speedboat, Chad practiced at several nationally-recognized firms, including MADE, M1DTW, Schuchart/Dow, and Radius Track. \
                 He is admitted to the ARE in New York and is a licensed general contractor in California. He lives in Silver Lake, Los Angeles.'
                 }, 
-                {text : 'Clara Dykstra is an architectural and urban designer with a background in anthropology. Her desire is to work meaningfully within the political, \
-                environmental, social, and financial forces that shape cities and buildings. She is dedicated to designing compelling and equitable spaces that create a \
-                positive impact at all scales. Clara has extensive experience working with NYC developers and with nonprofits clients to support their surrounding communities \
-                through the design and development of their landholdings. She holds a B.A. from UCLA and an M.Arch from Columbia University.'
-                },
-                {text : 'Hunter Knight is an architect with twenty years of experience on residential, commericial, and institutional projects in Los Angeles, New York, and Paris.\
-                 Previously he taught at the USC, Cal-Poly Pomona, Ecole Speciale d’Architecture, and SCI_Arc. He has practiced with Weather Proects and Morphosis Architects as lead \
-                 designer on several large-scale projects, including the Phare Tower in Paris and the Cornell Tech building in New York. He holds a B.Arch from the University of North \
-                 Carolina and an M.Arch from the Southern California Institute of Architecture, where he received the AIA Henry Adam’s AIA Gold Medal. R.A., CA: C 38635'
-                },
-                {text : 'Halle Doentiz has over ten years of structural engineering experience on a wide-range of institutional building types, including museums, hospitals, mixed-use \
-                high rises, university buildings, parking structures, and renovations. Utilizing her experience and passion for design, Halle approaches projects with a focus on performance, \
-                efficiency and innovation. She previously worked with Ghafari Associates in Michigan and LERA Consulting Structural Engineers in New York and Los Angeles. Halle holds a MSE and \
-                BSE in Civil Engineering with an emphasis on Structures from the University of Michigan. P.E., CA 91458, NY 093268'
-                }
+                {
+                name: 'First Last', 
+                text: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet justo nisl, vitae vehicula tortor rhoncus vitae. Curabitur vel \
+                dolor facilisis, vehicula mauris sit amet, mattis magna. Quisque sit amet leo a orci consectetur cursus at eu lorem. Pellentesque quis\
+                enim vitae nibh vehicula tincidunt. Nunc ac mauris eu quam vehicula tincidunt vitae tincidunt enim. Vestibulum diam ipsum, molestie a\
+                semper in, ullamcorper id nisi.'
+                }, 
+                {
+                name: 'First Last', 
+                text: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet justo nisl, vitae vehicula tortor rhoncus vitae. Curabitur vel \
+                dolor facilisis, vehicula mauris sit amet, mattis magna. Quisque sit amet leo a orci consectetur cursus at eu lorem. Pellentesque quis\
+                enim vitae nibh vehicula tincidunt. Nunc ac mauris eu quam vehicula tincidunt vitae tincidunt enim. Vestibulum diam ipsum, molestie a\
+                semper in, ullamcorper id nisi.'
+                }, 
+                {
+                name: 'First Last', 
+                text: ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet justo nisl, vitae vehicula tortor rhoncus vitae. Curabitur vel \
+                dolor facilisis, vehicula mauris sit amet, mattis magna. Quisque sit amet leo a orci consectetur cursus at eu lorem. Pellentesque quis\
+                enim vitae nibh vehicula tincidunt. Nunc ac mauris eu quam vehicula tincidunt vitae tincidunt enim. Vestibulum diam ipsum, molestie a\
+                semper in, ullamcorper id nisi.'
+                }, 
+                // {text : 'Clara Dykstra is an architectural and urban designer with a background in anthropology. Her desire is to work meaningfully within the political, \
+                // environmental, social, and financial forces that shape cities and buildings. She is dedicated to designing compelling and equitable spaces that create a \
+                // positive impact at all scales. Clara has extensive experience working with NYC developers and with nonprofits clients to support their surrounding communities \
+                // through the design and development of their landholdings. She holds a B.A. from UCLA and an M.Arch from Columbia University.'
+                // },
+                // {text : 'Hunter Knight is an architect with twenty years of experience on residential, commericial, and institutional projects in Los Angeles, New York, and Paris.\
+                //  Previously he taught at the USC, Cal-Poly Pomona, Ecole Speciale d’Architecture, and SCI_Arc. He has practiced with Weather Proects and Morphosis Architects as lead \
+                //  designer on several large-scale projects, including the Phare Tower in Paris and the Cornell Tech building in New York. He holds a B.Arch from the University of North \
+                //  Carolina and an M.Arch from the Southern California Institute of Architecture, where he received the AIA Henry Adam’s AIA Gold Medal. R.A., CA: C 38635'
+                // },
+                // {text : 'Halle Doentiz has over ten years of structural engineering experience on a wide-range of institutional building types, including museums, hospitals, mixed-use \
+                // high rises, university buildings, parking structures, and renovations. Utilizing her experience and passion for design, Halle approaches projects with a focus on performance, \
+                // efficiency and innovation. She previously worked with Ghafari Associates in Michigan and LERA Consulting Structural Engineers in New York and Los Angeles. Halle holds a MSE and \
+                // BSE in Civil Engineering with an emphasis on Structures from the University of Michigan. P.E., CA 91458, NY 093268'
+                // }
             ]
     },
         Friends : {
